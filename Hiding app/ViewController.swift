b8ed1c70e43d309edec3e81e3860041895ab14dc
Sourabh Nag <sourabhnag@Sourabhs-MacBook-Air.local>
2016-10-23 13:33:42 +0530

    added files for supercool

--- Hiding app/ViewController.swift
+++ Hiding app/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Hidingredimage(_ sender: UIButton) {
+        Image1.isHidden = true
+    }
+   
+    @IBAction func hidingBlueButton(_ sender: UIButton) {
+        image2.isHidden = true
+    }
 }
 
